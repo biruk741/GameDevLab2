2396cf4bee5a31fcf1ab3ee64f08e2e08a57d5da
Biruk <mengi024@morris.umn.edu>
2022-01-31 11:53:59 -0600

    added assets

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button openCreditsButton;
+    [SerializeField] private Button closeCreditsButton;
+    [SerializeField] private GameObject creditsUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(() => {
+            doSomething();
+        });
+        openCreditsButton.onClick.AddListener(() => {
+            creditsUI.SetActive(true);
+        });
+        closeCreditsButton.onClick.AddListener(() => {
+            creditsUI.SetActive(false);
+        });
+    }
+
+    private void doSomething()
+    {
+        LoadingScreen.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
